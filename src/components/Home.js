84e17fdd141357cd66d60466715aaeed8630d578
dinosaur96m <nccadmantinaos@gmail.com>
2021-12-12 23:53:11 -0500

    filter works with form params

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,59 +7,59 @@ function Home (props) {
 	//the below is an array of test data
 	let allItems = [
 			{
-			"._id": 1,
-			"CreatorId": 1,
-			"RecipientId": null ,
-			"Category": "Coat",
-			"Description": "This is a really nice coat, just don't need it anymore",
-			"Zipcode": 10017,
-			"imgurl": "",
-			"timestamp": 1
+			_id: 1,
+			creatorId: 1,
+			recipientId: null ,
+			category: "Coat",
+			description: "This is a really nice coat, just don't need it anymore",
+			zipcode: 10017,
+			imgurl: "",
+			timestamp: 1
 			},
 			{
-			"_id": 2,
-			"CreatorId": 1,
-			"RecipientId": null ,
-			"Category": "Household",
-			"Description": "test Queen size, unopend bedding set",
-			"Zipcode": 10017,
-			"imgurl": "",
-			"timestamp": 2
+			_id: 2,
+			creatorId: 1,
+			recipientId: null ,
+			category: "Household",
+			description: "test Queen size, unopend bedding set",
+			zipcode: 10017,
+			imgurl: "",
+			timestamp: 2
 			},
 			{
-				"_id": 3,
-				"CreatorId": 1,
-				"RecipientId": null ,
-				"Category": "Household",
-				"Description": "test Queen size, unopend bedding set",
-				"Zipcode": 10017,
-				"imgurl": "",
-				"timestamp": 2
+				_id: 3,
+				creatorId: 1,
+				recipientId: null ,
+				category: "Household",
+				description: "test Queen size, unopend bedding set",
+				zipcode: 10014,
+				imgurl: "",
+				timestamp: 2
 				},
 				{
-				"_id": 4,
-				"CreatorId": 1,
-				"RecipientId": null ,
-				"Category": "Accessories",
-				"Description": "unopened five pack of socks",
-				"Zipcode": 10017,
-				"imgurl": "",
-				"timestamp": 3
+				_id: 4,
+				creatorId: 1,
+				recipientId: null ,
+				category: "Accessories",
+				description: "unopened five pack of socks",
+				zipcode: 10017,
+				imgurl: "",
+				timestamp: 3
 				},
 				{
-				"_id": 5,
-				"CreatorId": 1,
-				"RecipientId": null ,
-				"Category": "Outdoor",
-				"Description": "10 foot hose",
-				"Zipcode": 10017,
-				"imgurl": "",
-				"timestamp": 3
+				_id: 5,
+				creatorId: 1,
+				recipientId: null ,
+				category: "Outdoor",
+				description: "10 foot hose",
+				zipcode: 10017,
+				imgurl: "",
+				timestamp: 3
 				}
 			
 	]
-	//set state that will hold all data that matches search
-
+	//TO DO: set state that will hold all data that matches search
+	const [searchResults, setSearchResults] = useState([])
 
 	//write a function that will set state accoring to form submission
 	const searchItems = (e) => {
@@ -69,21 +69,16 @@ function Home (props) {
 		console.log('zipcode: ', e.target.zipcode.value)
 		let zip = e.target.zipcode.value
 
-		// let results = 
+		/////the Below code is copy pasted from Fruit Filter, to refactor with our app
 
-		///////the Below code is copy pasted from Fruit Filter, to refactor with our app
-		// handleFilterChange = (e) => {
-		// 	//set up a filter that removes fruits that don't match the value in the input
-		// 	//referring to each fruit as f
-		// 	const filteredFrutisList = this.props.fruits.filter((f) => {
-		// 		return f.toLowerCase().includes(e.target.value.toLowerCase())
-		// 	})
-		// 	//then we will set state with the new array of fruits 
-		// 	console.log('this is the new list of fruits: ', filteredFrutisList)
-		// 	this.setState({
-		// 		fruitsToDisplay: filteredFrutisList
-		// 	})
-		// }
+		//set up a filter that removes fruits that don't match the value in the input
+		//referring to each fruit as f
+		const filteredListings = allItems.filter((l) => {
+			return l.description.toLowerCase().includes(srch.toLowerCase()) && l.zipcode.toString().includes(zip.toString())
+		})
+		//then we will set state with the new array of fruits 
+		console.log('search results: ', filteredListings)
+		setSearchResults(filteredListings)
 		/////////////////////////////////
 
 	}
