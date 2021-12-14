9c106ff471adbb134e18db12eedd0f52c090222c
gsdkira <larreakelly@gmail.com>
2021-12-13 21:39:38 -0500

    Profile.js created and rendering results

---
+++ src/components/Profile.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import { Route, Routes } from "react-router-dom";
+
+function Profile() {
+
+    // console.log('this are the props', props);
+    let [favorite, setFavorite] = useState ([])
+    // const handleClick = (item) => {
+    //     if(faves.indexOf(item)===1) {
+    //     //push to s tate
+    //     //let the first part be faves but at then end add a villager
+    //     setFaves([...faves, item])
+    //   }
+    //   }
+    useEffect(() => {
+        console.log('getting items')
+        getItems()
+      }, [])
+    
+      //get all listings from the db
+      const getItems = () => {
+        fetch('http://localhost:8000/items')
+        .then(response=>response.json())
+        .then(foundItems=>{
+          setFavorite(foundItems)
+          console.log('all Items: ',favorite.items )
+        })
+        .catch(err => {
+          console.log(err)
+        })
+      }
+    
+
+
+return (
+        <Routes>
+            <Route path = "/profile"  element={<Profile  /> }  />
+        </Routes>
+)
+}
+export default Profile
