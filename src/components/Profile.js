af1e6e871648b3b976ec29555f20e72114a9f2fa
gsdkira <larreakelly@gmail.com>
2021-12-16 17:14:26 -0500

    changes to form js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-import Form from './Form'
-=======
->>>>>>> f8f52e358c1baa8e43ab9653eac8be1885963262
 import { Link } from 'react-router-dom'
 import Form from './Form'
 const linkStyle = {
@@ -13,30 +9,14 @@ console.log('profile props', props);
 
   //route
   return (
-<<<<<<< HEAD
-    <>
-    <Form refreshItem ={props.refreshItem} user ={props.user} />
-=======
     <div>
       <Form refreshItem ={props.refreshItem} user ={props.user} />
->>>>>>> f8f52e358c1baa8e43ab9653eac8be1885963262
       <h1>Hello</h1>
       
     <Link to={{pathname: "/favorites"}}>
         My Favorites
-<<<<<<< HEAD
-      </Link>
-      <div>
-      <Link to={{pathname: "/edititem"}}>
-        Create a Listing
-      </Link>
-      </div>
-  
-    </>
-=======
       </Link >
    </div>
->>>>>>> f8f52e358c1baa8e43ab9653eac8be1885963262
   )
 }
 export default Profile
