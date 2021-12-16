6ea363f7d9147bb53e27855f608e75756804c096
gsdkira <larreakelly@gmail.com>
2021-12-16 16:12:32 -0500

    Edited create listing able to render and post with form

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,4 @@
-
+import Form from './Form'
 import { Link } from 'react-router-dom'
 const linkStyle = {
   color: 'black',
@@ -10,11 +10,17 @@ function Profile(props) {
   //route
   return (
     <>
+    <Form refreshItem ={props.refreshItem} user ={props.user} />
       <h1>Hello</h1>
       
     <Link to={{pathname: "/favorites"}}>
         My Favorites
       </Link>
+      <div>
+      <Link to={{pathname: "/edititem"}}>
+        Create a Listing
+      </Link>
+      </div>
   
     </>
   )
