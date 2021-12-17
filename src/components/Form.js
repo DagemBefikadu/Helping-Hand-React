f9743c15b3506b82c4715e553dd1509534f49422
gsdkira <larreakelly@gmail.com>
2021-12-17 09:31:59 -0500

    adding created listings

--- src/components/Form.js
+++ src/components/Form.js
@@ -61,7 +61,7 @@ function Form(props) {
 }
 
     return (
-   
+        
    
         <form onSubmit ={createItem}>
             <div>
--- src/components/profile/CreatedListings.js
+++ src/components/profile/CreatedListings.js
@@ -16,36 +16,36 @@ function CreatedListings(props) {
         })
             .then(foundUser => {
                 console.log('hello user created route', foundUser)
-                    const foundUserListing = foundUser.data.users.createdItems
-                    setListing(foundUserListing)
-                    // console.log('founduserListing', foundUserListing)
-                  })
-                  .catch((error) => {
-                    console.error(error)
+                const foundUserListing = foundUser.data.users.createdItems
+                setListing(foundUserListing)
+                // console.log('founduserListing', foundUserListing)
             })
-        }
+            .catch((error) => {
+                console.error(error)
+            })
+    }
 
-        const deleteCreatedItem = (createdId) => {
-            axios({
-                url: `http://localhost:8000/users/createdItems/${createdId}`,
-                method: 'DELETE',
-                headers: {
-                    Authorization: `Token token=${props.user.token}`,
-                }
-            })
+    const deleteCreatedItem = (createdId) => {
+        axios({
+            url: `http://localhost:8000/users/createdItems/${createdId}`,
+            method: 'DELETE',
+            headers: {
+                Authorization: `Token token=${props.user.token}`,
+            }
+        })
             .then(res => console.log('server response:', res))
-            .then(() => { 
-                getCreatedItems() 
+            .then(() => {
+                getCreatedItems()
             })
             .catch(err => console.log(err))
-        }
-        useEffect(() => {
-            getCreatedItems()
-            console.log('all of userListings: ', createdListing)
-        }, [])
+    }
+    useEffect(() => {
+        getCreatedItems()
+        console.log('all of userListings: ', createdListing)
+    }, [])
     return (
         <div>
-            <UserListing createdListing={createdListing} deleteCreatedItem={deleteCreatedItem}/>
+            <UserListing createdListing={createdListing} deleteCreatedItem={deleteCreatedItem} />
         </div>
     )
 }
