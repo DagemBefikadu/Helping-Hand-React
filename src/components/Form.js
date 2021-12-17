c1333c25f6f412527babd7592f7dbb25f7ffe71d
dinosaur96m <nccadmantinaos@gmail.com>
2021-12-17 11:45:44 -0500

    working code

--- src/components/Form.js
+++ src/components/Form.js
@@ -67,7 +67,7 @@ function Form(props) {
 
     return (
    
-        <form onSubmit ={createItem}>
+        <form onSubmit={createItem} enctype="multipart/form-data">
             <div>
                 <label htmlFor ='name'>item name:</label>
                 <input type='text' name='name' id='name' 
