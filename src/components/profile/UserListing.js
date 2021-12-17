94778f455919e5eb8f2d661fcae0140227b0d261
gsdkira <larreakelly@gmail.com>
2021-12-17 09:20:58 -0500

    Still trying to push to master

--- src/components/profile/UserListing.js
+++ src/components/profile/UserListing.js
@@ -1,20 +1,24 @@
 import React, { useState } from 'react'
 import ItemList from '../ItemList'
 import CreatedListings from './CreatedListings'
+import { Link } from 'react-router-dom'
 
-function UserListing (props) {
+function UserListing(props) {
     console.log('these are listing props', props)
-    const userListings = props.createdListing.map((u) =>{
+    const userListings = props.createdListing.map((u) => {
         console.log('map list', u)
-        return<div>
+        return <div>
             <ItemList listItem={u} />
             <button onClick={() => props.deleteCreatedItem(u._id)}>Delete Listing</button>
         </div>
     })
-    return(
+    return (
         <div>
             <h1>My Listings:</h1>
-            {userListings} 
+            {userListings}
+            <Link to={{ pathname: "/editForm" }}>
+                Create a Listing
+        </Link >
         </div>
     )
 }
